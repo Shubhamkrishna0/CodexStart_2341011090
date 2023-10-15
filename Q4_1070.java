a5f9020e0038514cca5ce7bf9962a12738636e94
Shubhamkrishna0 <krishnashubham09@gmail.com>
2023-10-15 16:36:03 +0530

    ADDING Q4

---
+++ Q4_1070.java
@@ -0,0 +1,31 @@
+//NAME:- SHUBHAM KRISHNA
+
+//REGISTRATION NO. :- 2341011090
+
+//Problem Statement Link :-https://cses.fi/problemset/task/1070
+
+import java.util.Scanner;
+
+public class Q4_1070 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        if (n == 1) {
+            System.out.println("1");
+        } else if (n <= 3) {
+            System.out.println("no solution");
+        } else {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 2; i <= n; i += 2) {
+                result.append(i).append(" ");
+            }
+            for (int i = 1; i <= n; i += 2) {
+                result.append(i).append(" ");
+            }
+            System.out.println(result);
+        }
+    }
+}
